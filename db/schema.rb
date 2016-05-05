d511121893b29ca1ec4a7f441f07439b16dcdeb5
jtam11 <justinktam@gmail.com>
2016-05-05 11:49:29 -0700

    migrated password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505170252) do
+ActiveRecord::Schema.define(version: 20160505182517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20160505170252) do
     t.string   "email"
     t.string   "current_city"
     t.string   "profile_pic"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
 
