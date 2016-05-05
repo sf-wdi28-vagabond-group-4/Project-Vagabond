33cc6e793de4559575b1f6b1be5768553387fe7e
jtam11 <justinktam@gmail.com>
2016-05-05 13:54:45 -0700

    Created edit/update view/action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city, :profile_pic)
   end
 
 end
