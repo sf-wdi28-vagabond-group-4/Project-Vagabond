f17ecbacf62944cf3247c4590804fba4e5bf50fd
cloverharvest <anna@schwabdev.com>
2016-05-05 16:13:43 -0700

    Adds paperclip functionality to edit user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city, :profile_pic)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city, :image)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "150x150#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
