6cbbfdabd23a7a0697769a26a005bce4de8bb8f6
jtam11 <justinktam@gmail.com>
2016-05-06 09:10:25 -0700

    Made cities view page

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,5 +5,24 @@ class CitiesController < ApplicationController
     render :index
   end
 
-  
+  def new
+    @city = City.new
+  end
+
+  def create
+    @city = City.create(city_params)
+    redirect_to city_path
+  end
+
+  def show
+    @city = City.find_by(params[:id])
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:city_name, :state, :country, :city_picture)
+  end
+
+
 end
