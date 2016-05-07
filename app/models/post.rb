4c044f5ae0c8d3e7277768d953c76aa3ec2b35d0
cloverharvest <anna@schwabdev.com>
2016-05-07 12:04:40 -0700

    Testing scenarios on title of post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :city
 
-  validates :title, length: { in: 1..200 }
-  validates :content, presence: true
+  # validates :title, length: { in: 1..200 }
+  # validates :content, presence: true
 end
