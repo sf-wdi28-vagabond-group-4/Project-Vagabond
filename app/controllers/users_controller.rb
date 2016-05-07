9a58bce1baaa984c6892289042738dc0f8339e6b
cloverharvest <anna@schwabdev.com>
2016-05-07 11:16:07 -0700

    Working email uniqueness with page staying in new user path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,30 +5,26 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-
+  def new
+    @user = User.new
+  end
 
   def create
-    @user = User.create(user_params)
-    if User.exists?(:email => @user.email)
+    @user = User.new(user_params)
+    if @user.save
       login(@user)
       redirect_to @user
     else
-      flash[:notice] = "Sign up not succcessful, please enter information."
       render :new
     end
   end
 
   def show
     @user = User.find_by_id(params[:id])
-  
+
     if current_user != @user
       redirect_to root_path
     end
-  end
-
-  def new
-    @user = User.new
-    render :new
   end
 
   def edit
@@ -44,8 +40,8 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city, :image)
-  end
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city, :image)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   validates :email, uniqueness: true
-  
 
   has_many :posts
   has_secure_password
