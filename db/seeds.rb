d8907bf435304ebedf61ed3431836ffb81db8e28
jtam11 <justinktam@gmail.com>
2016-05-07 17:07:43 -0700

    Added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,6 @@
-User.destroy_all
-
-User.create([
-  {
-    first_name: "Scott",
-    last_name: "Madrid",
-    email:  "samadrid92@gamil.com",
-    current_city: "Los Angeles",
-    profile_pic: "http://i.telegraph.co.uk/multimedia/archive/03235/potd-husky_3235255k.jpg"
-  },
-  {
-    first_name: "Justin",
-    last_name: "Tam",
-    email: "justin@hotmail.com",
-    current_city: "San Francisco",
-    profile_pic: "http://i.telegraph.co.uk/multimedia/archive/03235/potd-husky_3235255k.jpg"
-  }
-])
+12.times do
+	title = FFaker::Lorem.sentence
+  content = FFaker::Lorem.paragraph
+  user_id = rand(4) + 1
+  Post.create({title: title, content: content, city_id: 1, user_id: user_id})
+end
