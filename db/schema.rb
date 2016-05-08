f0d4194434a74a6aa6faa61fd0f3252c7310d06b
jtam11 <justinktam@gmail.com>
2016-05-08 02:17:39 -0700

    Created migration to add header to cities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507004611) do
+ActiveRecord::Schema.define(version: 20160508090137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20160507004611) do
     t.string   "state"
     t.string   "country"
     t.string   "city_picture"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "header_picture"
   end
 
   create_table "posts", force: :cascade do |t|
