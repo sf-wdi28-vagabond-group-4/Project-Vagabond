1a540dc4b88b36bcb1cf1ba516e337c6e0f0c8c0
jtam11 <justinktam@gmail.com>
2016-05-09 02:31:38 -0700

    Added comment routes and created a controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508090137) do
+ActiveRecord::Schema.define(version: 20160509090156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 20160508090137) do
     t.datetime "updated_at",     null: false
     t.string   "header_picture"
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -53,6 +64,8 @@ ActiveRecord::Schema.define(version: 20160508090137) do
     t.datetime "image_updated_at"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "cities"
   add_foreign_key "posts", "users"
 end
