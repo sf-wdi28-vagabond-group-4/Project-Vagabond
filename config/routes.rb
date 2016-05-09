306fa8e166e5d879ed44cf1dc4776fdd3f1c3b9a
jtam11 <justinktam@gmail.com>
2016-05-09 04:12:05 -0700

    Added edit/update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :cities, only: [:index, :show] do
     resources :posts do
-      resources :comments, only: [:create, :destroy]
+      resources :comments, except: [:index, :show]
     end
   end
 
