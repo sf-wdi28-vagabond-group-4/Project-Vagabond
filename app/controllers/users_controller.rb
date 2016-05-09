0cfefdecf4a48c9f2f5dc4335628fba8ee34a3fe
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-05-08 20:59:23 -0700

    some changes for auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = "New User Created!"
       login(@user)
       redirect_to @user
     else
