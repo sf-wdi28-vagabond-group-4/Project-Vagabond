ecc517c0cd99038a5ba54b7564433510a7cedeac
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-05-09 10:26:30 -0700

    mailer not quite working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.welcome_email(@user).deliver
       flash[:notice] = "New User Created!"
       login(@user)
       redirect_to @user
